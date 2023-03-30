1d375ad5cb1c664cbde1f7e1a0cb5201de8933ba
0n1cOn3 <0n1cOn3@gmx.ch>
2023-03-30 14:58:32 +0200

    Updated Code Basement for python < 3.9

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-from ESS.shell import Shell
-
+from ess.shell import Shell
 
 if __name__ == '__main__':
 
@@ -8,11 +7,11 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         if len(sys.argv) > 2:
-            print "Bad argument, usage is: main.py FILEPATH"
-            exit(-1)
+            print("Bad argument, usage is: main.py FILEPATH")
+            sys.exit(-1)
 
         shell.load_from_file(sys.argv[1])
     try:
         shell.start()
     except KeyboardInterrupt:
-        exit(0)
+        sys.exit(0)
